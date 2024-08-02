31982cb77b1a1074f2e8230c0b985804b3451b3a
Avyaan <Avyaan@gmail.com>
2024-08-02 10:46:15 +0530

    second file

---
+++ my first code/my first code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello word");
+            Console.ReadKey();
+        }
+    }
+}
