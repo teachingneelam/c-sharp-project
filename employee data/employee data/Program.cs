564c94d0867770d1bb82df14ae5490dc8b6c9352
Avyaan <Avyaan@gmail.com>
2024-08-02 11:35:53 +0530

    employee file

---
+++ employee data/employee data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee name is : Neelam");
+            Console.WriteLine("Employee work : service");
+            Console.WriteLine("Employee item : hardwork");
+            Console.ReadLine();
+
+        }
+    }
+}
